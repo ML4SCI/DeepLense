034980858d0baa4e6d05970fa7be5ae55add5a2e
Saranga7 <saranga.boo@gmail.com>
2022-07-27 15:43:13 +0530

    Model3 classification complete

--- Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_II/config.py
+++ Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_II/config.py
@@ -1,7 +1,8 @@
-EPOCHS = 2
+EPOCHS = 25
 LEARNING_RATE = 3e-4
 BATCH_SIZE = 128
+SAVE_MODEL = False
 
 MODEL_PATH = 'model_ckpts\model2_EfficientNetB1Backbone.pth'
-TRAIN_DATA_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\Work\Model_I_subset\*\*'
-TEST_DATA_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\Work\Model_I_test_subset\*\*'
+TRAIN_DATA_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\Work\Model_II_subset\*\*'
+TEST_DATA_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\Work\Model_II_test\*\*'
