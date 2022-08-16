af49438c1264c1425ae9d60415119ec444f4e985
Saranga7 <saranga.boo@gmail.com>
2022-08-16 07:34:51 +0530

    Classifcation minor changes

--- Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_III/utils.py
+++ Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_III/utils.py
@@ -1,4 +1,6 @@
 import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 def calculate_accuracy(y_pred, y_truth):
     y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
@@ -11,3 +13,11 @@ def calculate_accuracy(y_pred, y_truth):
     return acc  
 
 device ='cuda' if torch.cuda.is_available() else 'cpu'
+
+#pre-processing transformation
+transforms = A.Compose(
+        [
+            A.CenterCrop(height = 50, width = 50, p=1.0),
+            ToTensorV2()
+        ]
+    )
