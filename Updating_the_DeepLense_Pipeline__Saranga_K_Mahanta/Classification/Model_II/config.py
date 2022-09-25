d2989aa185dcbd28722cea01882397981362106b
Saranga7 <saranga.boo@gmail.com>
2022-09-25 15:53:05 +0200

    Classification Models README completed

--- Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_II/config.py
+++ Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_II/config.py
@@ -1,6 +1,7 @@
 EPOCHS = 25
 LEARNING_RATE = 3e-4
 BATCH_SIZE = 128
+LOAD_PRETRAINED_MODEL = False
 SAVE_MODEL = False
 
 MODEL_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\All MODELS\Classification\Model_II\model2_EfficientNetB1Backbone.pth'
--- Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_III/config.py
+++ Updating_the_DeepLense_Pipeline__Saranga_K_Mahanta/Classification/Model_III/config.py
@@ -1,6 +1,7 @@
 EPOCHS = 25
 LEARNING_RATE = 3e-4
 BATCH_SIZE = 128
+LOAD_PRETRAINED_MODEL = False
 SAVE_MODEL = False
 
 MODEL_PATH = r'C:\Users\Saranga\Desktop\ML4SCI\All MODELS\Classification\Model_III\model3_EfficientNetB1Backbone.pth'
