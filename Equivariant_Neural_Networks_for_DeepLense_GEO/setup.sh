9f5ad845e02a1f73002ccd7b4fde9875aae2fc9e
Geo Jolly <geojollyc@gmail.com>
2023-09-25 23:52:55 +0530

    Add codes for regression task

--- Equivariant_Neural_Networks_for_DeepLense_GEO/setup.sh
+++ Equivariant_Neural_Networks_for_DeepLense_GEO/setup.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 sudo /opt/conda/bin/jupyter nbconvert --execute $1 --to notebook --inplace
 sudo /opt/conda/bin/jupyter notebook --ip='0.0.0.0' --allow-root
+sleep 3200
