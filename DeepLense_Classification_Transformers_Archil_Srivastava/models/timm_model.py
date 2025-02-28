80ff75e43199ad319eff29fe4ca4f9b029175a67
Ankita Ghavate <144261424+Ankitaghavate@users.noreply.github.com>
2025-02-28 23:41:39 +0530

    Update timm_model.py

--- DeepLense_Classification_Transformers_Archil_Srivastava/models/timm_model.py
+++ DeepLense_Classification_Transformers_Archil_Srivastava/models/timm_model.py
@@ -1,62 +1,26 @@
 import torch
 from torch.nn import (
     Sequential,
-    ModuleList,
     Linear,
     PReLU,
     BatchNorm1d,
     Dropout,
-    Identity,
-    LazyLinear,
     Flatten,
 )
 import timm
 
-
 class TimmModelSimple(torch.nn.Module):
-    """
-    Timm model backbone with a single layer softmax classifier
+    def __init__(self, name, in_chans=1, num_classes=3, pretrained=True, tune=True):
+        super().__init__()
+        self.backbone = timm.create_model(name, pretrained=pretrained, in_chans=in_chans, num_classes=num_classes)
 
-    Parameters
-    ----------
-    name : str
-        Name of the Timm model
-    in_chans : int
-        Input Channels
-    num_classes : int
-        Number of classes
-    pretrained : bool
-        Whether to pick pretrained Timm model or train from scratch. Default is True
-    tune : bool
-        Whether to tune the whole Timm model or just train the classifier layer. Default is True
-    """
-
-    def __init__(
-        self,
-        name,
-        *args,
-        in_chans=1,
-        num_classes=3,
-        pretrained=True,
-        tune=True,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self.backbone = timm.create_model(
-            name, pretrained=pretrained, in_chans=in_chans, num_classes=num_classes
-        )
+        # Freeze or tune entire model
         for param in self.backbone.parameters():
-            param.requires_grad = tune
-        if (
-            type(self.backbone.get_classifier()) == tuple
-            or type(self.backbone.get_classifier()) == list
-        ):
-            for component in self.backbone.get_classifier():
-                for param in component.parameters():
-                    param.requires_grad = True
-        else:
-            for param in self.backbone.get_classifier().parameters():
-                param.requires_grad = True
+            param.requires_grad = tune  
+
+        # Ensure classifier layers are always trainable
+        for param in self.backbone.get_classifier().parameters():
+            param.requires_grad = True
 
         self.num_classes = num_classes
 
@@ -66,36 +30,33 @@ class TimmModelSimple(torch.nn.Module):
     def forward(self, img):
         return self.backbone(img)
 
+    def print_model_summary(self, input_size=(1, 1, 224, 224)):
+        from torchinfo import summary
+        summary(self, input_size)
 
 class TimmModelComplex(TimmModelSimple):
-    """
-    Timm model backbone with a multi-layer classifier appended to it
-
-    Parameters
-    ----------
-    args : Arguments to the base class (See TimmModelSimple for the parameter description)
-    dropout_rate : float
-        Dropout rate for the classifier
-    """
-
     def __init__(self, *args, dropout_rate=0.5, **kwargs):
         super().__init__(*args, **kwargs)
-        # in_features = self.backbone.get_classifier().in_features
-        # self.backbone.reset_classifier(0)
+        self.dropout_rate = dropout_rate
+
+        # Remove old classifier
+        self.backbone.reset_classifier(0)
+
+        # Multi-layer classifier
         self.classifier = Sequential(
             Flatten(),
-            LazyLinear(1024),
+            Linear(self.get_representation_features(), 1024),
             PReLU(),
             BatchNorm1d(1024),
-            Dropout(0.5),
+            Dropout(self.dropout_rate),
             Linear(1024, 512),
-            BatchNorm1d(512),
             PReLU(),
-            Dropout(0.5),
+            BatchNorm1d(512),
+            Dropout(self.dropout_rate),
             Linear(512, 128),
             PReLU(),
             BatchNorm1d(128),
-            Dropout(0.3),
+            Dropout(self.dropout_rate * 0.6),
             Linear(128, self.num_classes),
         )
 
